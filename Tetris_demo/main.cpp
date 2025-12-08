0e821157ebd9afd106f6a50bc1e4c7d06f354dea
Huy Phong <24521344@gm.uit.edu.vn>
2025-12-08 18:02:08 +0700

    Chuyển CTDL Block thành class Block

--- Tetris_demo/main.cpp
+++ Tetris_demo/main.cpp
@@ -1,15 +1,114 @@
-# include <iostream>
-# include <conio.h>
-# include <windows.h>
-# include <ctime>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <vector>
+#include <ctime>
 
 using namespace std;
 
-# define H 20
-# define W 15
+#define HEIGHT 20
+#define WIDTH 15
+#define BLOCK_SIZE 4
+
+char board[HEIGHT][WIDTH] = {} ;
+
+class BaseBlock {
+public:
+    int x;
+    int y;
+    vector<vector<char>> shape;
+
+    BaseBlock() {
+        x = WIDTH / 2 - 2;
+        y = 0;
+        shape = vector<vector<char>>(BLOCK_SIZE, vector<char>(BLOCK_SIZE, ' '));
+    }
+
+    virtual ~BaseBlock() {}
+
+    virtual void rotate() {
+        vector<vector<char>> tmp = shape;
+        vector<vector<char>> rot = vector<vector<char>>(BLOCK_SIZE, vector<char>(BLOCK_SIZE, ' '));
+
+        for (int i = 0; i < BLOCK_SIZE; i++)
+            for (int j = 0; j < BLOCK_SIZE; j++)
+                rot[j][BLOCK_SIZE - i - 1] = tmp[i][j];
+
+        for (int i = 0; i < BLOCK_SIZE; i++)
+            for (int j = 0; j < BLOCK_SIZE; j++) {
+                if (rot[i][j] == ' ')
+                    continue;
+
+                int tx = x + j;
+                int ty = y + i;
+
+                if (tx < 1
+                 || tx >= WIDTH - 1
+                 || ty >= HEIGHT - 1
+                 || board[ty][tx] != ' ')
+                    return;
+        }
+
+        shape = rot;
+    }
+};
+
+class BlockI : public BaseBlock {
+public:
+    BlockI() {
+        shape[1][0] = 'I'; shape[1][1] = 'I'; shape[1][2] = 'I'; shape[1][3] = 'I';
+    }
+};
+
+class BlockO : public BaseBlock {
+public:
+    BlockO() {
+        shape[1][1] = 'O'; shape[1][2] = 'O';
+        shape[2][1] = 'O'; shape[2][2] = 'O';
+    }
+};
+
+class BlockT : public BaseBlock {
+public:
+    BlockT() {
+                           shape[1][1] = 'T';
+        shape[2][0] = 'T'; shape[2][1] = 'T'; shape[2][2] = 'T';
+    }
+};
+
+class BlockL : public BaseBlock {
+public:
+    BlockL() {
+                                              shape[1][2] = 'L';
+        shape[2][0] = 'L'; shape[2][1] = 'L'; shape[2][2] = 'L';
+    }
+};
+
+class BlockJ : public BaseBlock {
+public:
+    BlockJ() {
+        shape[1][0] = 'J';
+        shape[2][0] = 'J'; shape[2][1] = 'J'; shape[2][2] = 'J';
+    }
+};
+
+class BlockS : public BaseBlock {
+public:
+    BlockS() {
+                           shape[1][1] = 'S'; shape[1][2] = 'S';
+        shape[2][0] = 'S'; shape[2][1] = 'S';
+    }
+};
+
+class BlockZ : public BaseBlock {
+public:
+    BlockZ() {
+        shape[1][0] = 'Z'; shape[1][1] = 'Z';
+                           shape[2][1] = 'Z'; shape[2][2] = 'Z';
+    }
+};
 
 int gameSpeed = 200;
-char board[H][W] = {} ;
 char blocks[][4][4] = {
         {{' ','I',' ',' '},
          {' ','I',' ',' '},
@@ -45,7 +144,6 @@ char blocks[][4][4] = {
          {'Z','Z',' ',' '},
          {' ','Z','Z',' '},
          {' ',' ',' ',' '}}
-
 };
 
 int x = 4;
@@ -60,7 +158,7 @@ void gotoxy(int x, int y) {
 void boardDelBlock(){
     for (int i = 0 ; i < 4 ; i++)
         for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' && y+j < H)
+            if (blocks[b][i][j] != ' ' && y+j < HEIGHT)
                 board[y+i][x+j] = ' ';
 }
 
@@ -72,16 +170,16 @@ void block2Board(){
 }
 
 void initBoard(){
-    for (int i = 0 ; i < H ; i++)
-        for (int j = 0 ; j < W ; j++)
-            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
+    for (int i = 0 ; i < HEIGHT ; i++)
+        for (int j = 0 ; j < WIDTH ; j++)
+            if ((i==HEIGHT-1) || (j==0) || (j == WIDTH-1)) board[i][j] = '#';
             else board[i][j] = ' ';
 }
 
 void draw(){
     gotoxy(0,0);
-    for (int i = 0 ; i < H ; i++, cout<<endl)
-        for (int j = 0 ; j < W ; j++)
+    for (int i = 0 ; i < HEIGHT ; i++, cout<<endl)
+        for (int j = 0 ; j < WIDTH ; j++)
             cout<<board[i][j];
 }
 
@@ -91,83 +189,46 @@ bool canMove(int dx, int dy){
             if (blocks[b][i][j] != ' '){
                 int tx = x + j + dx;
                 int ty = y + i + dy;
-                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
+                if ( tx<1 || tx >= WIDTH-1 || ty >= HEIGHT-1) return false;
                 if ( board[ty][tx] != ' ') return false;
             }
     return true;
 }
 
-<<<<<<< HEAD
 void animateLineClear(int line) {
     // Thay đổi các ô trong hàng thành ký tự particle
-    for (int k = 1; k < W-1 ; k++ ) {
+    for (int k = 1; k < WIDTH-1 ; k++ ) {
         board[line][k] = '*'; // Ký tự nhấp nháy/particle
     }
     draw();
     _sleep(100); // Dừng ngắn để người dùng thấy hiệu ứng
-=======
-void increaseSpeed()
-{
-    if(gameSpeed > 50) {
-        gameSpeed -= 10;
-    }
->>>>>>> origin/Speed_and_score
 }
 
 void removeLine(){
     int j;
 
-    for (int i = H - 2; i > 0 ; i-- ){
-        for (j = 0; j < W - 1 ; j++)
+    for (int i = HEIGHT - 2; i > 0 ; i-- ){
+        for (j = 0; j < WIDTH - 1 ; j++)
             if (board[i][j] == ' ')
                 break;
 
-        if (j == W - 1){
+        if (j == WIDTH - 1){
             animateLineClear(i);
 
             for (int ii = i; ii >0 ; ii-- )
-                for (int j = 0; j < W-1 ; j++ ) board[ii][j] = board[ii-1][j];
+                for (int j = 0; j < WIDTH-1 ; j++ ) board[ii][j] = board[ii-1][j];
 
             i++;
             draw();
-            increaseSpeed();
-            _sleep(gameSpeed);
+            _sleep(200);
         }
-<<<<<<< HEAD
     }
 }
 
 void increaseSpeed() {
     if(gameSpeed > 50) {
         gameSpeed -= 5;
-=======
->>>>>>> origin/Speed_and_score
     }
-}
-
-void rotateBlock() {
-    char rot[4][4], tmp[4][4];
-
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            tmp[i][j] = blocks[b][i][j];
-
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            rot[j][3 - i] = tmp[i][j];
-
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            if (rot[i][j] != ' ') {
-                int tx = x + j;
-                int ty = y + i;
-                if (tx < 1 || tx >= W - 1 || ty >= H - 1) return;
-                if (board[ty][tx] != ' ') return;
-            }
-
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            blocks[b][i][j] = rot[i][j];
 }
 
 void hideCursor() {
@@ -180,12 +241,12 @@ void hideCursor() {
 
 int main() {
     srand(time(0));
+    b = rand() % 2;
 
-    b = rand() % 7;
     hideCursor();
     system("cls");
-    initBoard();
 
+    initBoard();
     int timer = 0;
 
     while (1){
@@ -206,7 +267,7 @@ int main() {
                 block2Board();
                 removeLine();
                 increaseSpeed();
-                x = 5; y = 0; b = rand() % 7;
+                x = 5; y = 0; b = rand() % 2;
             }
 
             timer = 0;
@@ -214,13 +275,9 @@ int main() {
 
         block2Board();
         draw();
-<<<<<<< HEAD
 
         _sleep(50);
         timer += 50;
-=======
-        _sleep(gameSpeed);
->>>>>>> origin/Speed_and_score
     }
 
     return 0;
