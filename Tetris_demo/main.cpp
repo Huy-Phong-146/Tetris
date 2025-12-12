837d37aca7cc9f5c9d6f17112b26aecfeba9a8bc
Le Van Huu Phuc <24521381@gm.uit.edu.vn>
2025-12-12 15:16:52 +0700

    Chỉnh sửa block có dạng khối, đã có màu cơ bản

--- Tetris_demo/main.cpp
+++ Tetris_demo/main.cpp
@@ -14,6 +14,18 @@ using namespace std;
 #define D_SPEED_DECREASE 10
 
 //=============================
+// Thêm màu cho các khối
+//=========================
+enum BlockColor {
+    CYAN = 11,    // Block I
+    YELLOW = 14,  // Block O
+    PURPLE = 13,  // Block T
+    ORANGE = 12,  // Block L
+    BLUE = 9,     // Block J
+    GREEN = 10,   // Block S
+    RED = 12      // Block Z
+};
+//=============================
 // BaseBlock Class and Concrete Classes
 //=============================
 class BaseBlock {
@@ -21,6 +33,7 @@ public:
     int x;
     int y;
     vector<vector<char>> shape;
+    char color;
 
     BaseBlock() {
         x = WIDTH / 2 - 2;
@@ -60,55 +73,69 @@ public:
 class BlockI : public BaseBlock {
 public:
     BlockI() {
-        shape[1][0] = 'I'; shape[1][1] = 'I'; shape[1][2] = 'I'; shape[1][3] = 'I';
+        char c = 219;
+        color = CYAN;
+        shape[1][0] = c; shape[1][1] = c; shape[1][2] = c; shape[1][3] = c;
     }
 };
 
 class BlockO : public BaseBlock {
 public:
     BlockO() {
-        shape[1][1] = 'O'; shape[1][2] = 'O';
-        shape[2][1] = 'O'; shape[2][2] = 'O';
+        char c = 219;
+        color = YELLOW;
+        shape[1][1] = c; shape[1][2] = c;
+        shape[2][1] = c; shape[2][2] = c;
     }
 };
 
 class BlockT : public BaseBlock {
 public:
     BlockT() {
-                           shape[1][1] = 'T';
-        shape[2][0] = 'T'; shape[2][1] = 'T'; shape[2][2] = 'T';
+        char c = 219;
+        color = PURPLE;
+                           shape[1][1] = c;
+        shape[2][0] = c; shape[2][1] = c; shape[2][2] = c;
     }
 };
 
 class BlockL : public BaseBlock {
 public:
     BlockL() {
-                                              shape[1][2] = 'L';
-        shape[2][0] = 'L'; shape[2][1] = 'L'; shape[2][2] = 'L';
+        char c = 219;
+        color = ORANGE;
+                                              shape[1][2] = c;
+        shape[2][0] = c; shape[2][1] = c; shape[2][2] = c;
     }
 };
 
 class BlockJ : public BaseBlock {
 public:
     BlockJ() {
-        shape[1][0] = 'J';
-        shape[2][0] = 'J'; shape[2][1] = 'J'; shape[2][2] = 'J';
+        char c = 219;
+        color = BLUE;
+        shape[1][0] = c;
+        shape[2][0] = c; shape[2][1] = c; shape[2][2] = c;
     }
 };
 
 class BlockS : public BaseBlock {
 public:
     BlockS() {
-                           shape[1][1] = 'S'; shape[1][2] = 'S';
-        shape[2][0] = 'S'; shape[2][1] = 'S';
+        char c = 219;
+        color = GREEN;
+                           shape[1][1] = c; shape[1][2] = c;
+        shape[2][0] = c; shape[2][1] = c;
     }
 };
 
 class BlockZ : public BaseBlock {
 public:
     BlockZ() {
-        shape[1][0] = 'Z'; shape[1][1] = 'Z';
-                           shape[2][1] = 'Z'; shape[2][2] = 'Z';
+        char c = 219;
+        color = RED;
+        shape[1][0] = c; shape[1][1] = c;
+                           shape[2][1] = c; shape[2][2] = c;
     }
 };
 
@@ -118,13 +145,17 @@ public:
 class Board {
 public:
     vector<vector<char>> grid;
-
+    vector<vector<int>> colorGrid; // Thêm lưới màu
     Board() {
         grid = vector<vector<char>>(HEIGHT - 1, vector<char>(WIDTH, ' '));
+         colorGrid = vector<vector<int>>(HEIGHT - 1, vector<int>(WIDTH, 7)); // mặc định là màu trắng
         grid.emplace_back(vector<char>(WIDTH, '#'));
+        colorGrid.emplace_back(vector<int>(WIDTH, 7));
 
-        for (vector<char>& row : grid)
-            row.front() = row.back() = '#';
+       for (int i = 0; i < HEIGHT; i++) {
+            grid[i][0] = grid[i][WIDTH-1] = '#';
+            colorGrid[i][0] = colorGrid[i][WIDTH-1] = 7;
+        }
     }
 
     void gotoxy(int x, int y) {
@@ -134,10 +165,13 @@ public:
 
     void draw() {
         gotoxy(0, 0);
-
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
         for (int i = 0; i < HEIGHT; i++, cout << endl)
-            for (int j = 0; j < WIDTH; j++)
+            for (int j = 0; j < WIDTH; j++){
+                SetConsoleTextAttribute(hConsole, colorGrid[i][j]); // set màu khi vẽ
                 cout << grid[i][j];
+            }
+            SetConsoleTextAttribute (hConsole, 7); // reset lại màu trắng
     }
 
     void boardDeleteBlock(BaseBlock* currBlock) {
@@ -150,8 +184,12 @@ public:
     void blockToBoard(BaseBlock* currBlock){
         for (int i = 0 ; i < BLOCK_SIZE; i++)
             for (int j = 0 ; j < BLOCK_SIZE; j++)
-                if (currBlock->shape[i][j] != ' ')
+                if (currBlock->shape[i][j] != ' '){
                     grid[currBlock->y + i][currBlock->x + j] = currBlock->shape[i][j];
+                    // Gắn màu từ lưới vào các ô
+                     int tx = currBlock->x + j;
+                     int ty = currBlock->y + i;
+                    colorGrid[ty][tx] = currBlock->color;}
     }
 
     bool canMove(int dx, int dy, BaseBlock* currBlock) {
@@ -172,9 +210,9 @@ public:
     }
 
     void animateLineClear(int line) {
-        for (int k = 1; k < WIDTH - 1; k++)
+        for (int k = 1; k < WIDTH - 1; k++){
             grid[line][k] = '*';
-
+            colorGrid [line][k] =15;}
         draw();
         _sleep(100);
     }
@@ -197,8 +235,10 @@ public:
             hasLineClear = true;
 
             for (int ii = i; ii > 0; ii--)
-                for (int k = 0; k < WIDTH - 1; k++)
+                for (int k = 0; k < WIDTH - 1; k++){
                     grid[ii][k] = grid[ii - 1][k];
+                    colorGrid[ii][k] = colorGrid[ii - 1][k]; // dịch màu
+                }
 
             i++;
             draw();
@@ -295,6 +335,8 @@ public:
 };
 
 int main() {
+     SetConsoleOutputCP(437);
+    SetConsoleCP(437);
     srand(time(0));
     TetrisGame tetris;
     tetris.run();
